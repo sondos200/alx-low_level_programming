2d20e1a809c40eb90d28a588dec86ff080da04a9
sondos200 <sondoshossameldein@gmail.com>
2023-03-17 05:59:05 -0700

    done

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, and then in uppercase.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	char letter;
@@ -6,13 +12,10 @@ int main(void)
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 
-
-        for (letter = 'A'; letter <= 'Z'; letter++)
+	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
 
+	putchar('\n');
 
-    putchar('\n');
-
-
-    return (0);
+	return (0);
 }
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,11 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0,
+ *        only using putchar and without char variables.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int i;
-	for (i = 0;i < 10;i++) 
-	{
-             putchar((i % 10) + '0');
-	}
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
 	putchar('\n');
 
 	return (0);
